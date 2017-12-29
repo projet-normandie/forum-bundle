62830853ad677546ffeb3b0a9e8be8f59698ab14
magicbart <magicbart@gmail.com>
2017-12-29 15:46:26 +0100

    Add category

---
+++ Repository/CategoryRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ProjetNormandie\ForumBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Specific repository that serves the Badge entity.
+ */
+class CategoryRepository extends EntityRepository
+{
+}
