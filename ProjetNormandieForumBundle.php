712d2bb2059a763cb66346941e04e874a211ee6f
magicbart <magicbart@gmail.com>
2017-12-29 12:41:45 +0100

    Init forum bundle

---
+++ ProjetNormandieForumBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ProjetNormandie\ForumBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Specific implementation of the BundleInterface for the BadgeBundle of the ProjetNormandie.
+ */
+class ProjetNormandieForumBundle extends Bundle
+{
+}
