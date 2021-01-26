2994f576aa01ed578ffed1129813ee6ed6be2f10
magicbart <magicbart@gmail.com>
2021-01-26 20:32:56 +0100

    Add some queries to update data

---
+++ Resources/sql/update.sql
@@ -0,0 +1,16 @@
+-- TOPIC --
+UPDATE forum_topic t
+SET t.idMessageMax = (SELECT MAX(id) FROM forum_message WHERE idTopic = t.id);
+
+UPDATE forum_topic t
+SET t.nbMessage = (SELECT COUNT(id) FROM forum_message WHERE idTopic = t.id);
+
+-- FORUM --
+UPDATE forum_forum f
+SET f.idMessageMax = (SELECT MAX(idMessageMax) FROM forum_topic WHERE idForum = f.id);
+
+UPDATE forum_forum f
+SET f.nbTopic = (SELECT COUNT(id) FROM forum_topic WHERE idForum = f.id);
+
+UPDATE forum_forum f
+SET f.nbMessage = (SELECT SUM(nbMessage) FROM forum_topic WHERE idForum = f.id);
