82cfb1f622a457e68f342bfa49fc676ab443f57a
magicbart <magicbart@gmail.com>
2021-05-27 14:19:50 +0200

    Fix init user with native sql query

--- Repository/ForumUserRepository.php
+++ Repository/ForumUserRepository.php
@@ -2,12 +2,23 @@
 
 namespace ProjetNormandie\ForumBundle\Repository;
 
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityRepository;
+use VideoGamesRecords\CoreBundle\Entity\Chart;
 
 /**
  * Specific repository that serves the Forum entity.
  */
 class ForumUserRepository extends EntityRepository
 {
-
+    /**
+     * @param $user
+     * @throws Exception
+     */
+    public function init($user)
+    {
+        $query ="INSERT INTO forum_forum_user (idForum, idUser)
+                SELECT id, :idUser FROM forum_forum";
+        $this->_em->getConnection()->executeStatement($query, array('idUser' => $user->getId()));
+    }
 }
