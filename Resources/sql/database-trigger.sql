3830fd1f8f1882f089ac50370c8c4d4b4eee30d3
magicbart <magicbart@gmail.com>
2021-06-11 15:26:15 +0200

    Update trigger

--- Resources/sql/database-trigger.sql
+++ Resources/sql/database-trigger.sql
@@ -10,7 +10,6 @@ BEGIN
 END //
 delimiter ;
 
-
 -- Topic
 delimiter //
 DROP TRIGGER IF EXISTS `forumTopicAfterInsert`//
@@ -33,39 +32,6 @@ BEGIN
     INSERT INTO forum_forum_user (idUser, idForum, boolRead)
     SELECT DISTINCT idUser, NEW.id, 1
     FROM forum_forum_user;
-END //
-delimiter ;
-
-
-delimiter //
-DROP TRIGGER IF EXISTS `forumTopicUserAfterUpdate`//
-CREATE TRIGGER forumTopicUserAfterUpdate AFTER UPDATE ON forum_topic_user
-    FOR EACH ROW
-BEGIN
-    IF OLD.boolRead != NEW.boolRead THEN
-        SELECT idForum INTO @idForum FROM forum_topic WHERE id = NEW.idTopic;
-
-        IF NEW.boolRead = 0 THEN
-            UPDATE forum_forum_user
-            SET boolRead = 0
-            WHERE idUser = NEW.idUser
-                    AND idForum = @idForum;
-        ELSE
-            SELECT COUNT(forum_topic.id) INTO @nbTopicNotRead
-                FROM forum_topic_user, forum_topic
-                WHERE forum_topic_user.idTopic = forum_topic.id
-                AND idForum = @idForum
-                AND forum_topic_user.idUser = NEW.idUser
-                AND boolRead = 0;
-
-            IF (@nbTopicNotRead = 0) THEN
-                UPDATE forum_forum_user
-                SET boolRead = 1
-                WHERE idUser = NEW.idUser
-                AND idForum = @idForum;
-            END IF;
-        END IF;
-    END IF;
 END //
 delimiter ;
 
